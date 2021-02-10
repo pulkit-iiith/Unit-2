81bc81c0014c0ebfee5598ae5fc87b65e1b28f73
Adrijac3 <adrchk@gmail.com>
2021-02-10 08:57:25 +0530

    removed depracated function

--- Refactored Code/Views/ControlDeskView.java
+++ Refactored Code/Views/ControlDeskView.java
@@ -131,7 +131,7 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
         win.setLocation(
                 screenSize.width / 2 - win.getSize().width / 2,
                 screenSize.height / 2 - win.getSize().height / 2);
-        win.show();
+        win.setVisible(true);
     }
 
     /**
@@ -148,7 +148,7 @@ public class ControlDeskView implements ActionListener, ControlDeskObserver {
             controlDesk.assignLane();
         }
         if (e.getSource().equals(finished)) {
-            win.hide();
+            win.setVisible(false);
             System.exit(0);
         }
     }
