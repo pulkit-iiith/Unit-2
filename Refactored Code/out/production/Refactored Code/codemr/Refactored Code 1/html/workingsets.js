bf97b5b3fada13450fe6411e825cb7bc251cb3f7
gaurav batra <batragaurav2616@gmail.com>
2021-02-12 00:49:50 +0530

    Added Sequence Diagrams for refractored code

---
+++ Refactored Code/out/production/Refactored Code/codemr/Refactored Code 1/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: 'Refactored Code', path:'refactored-code'}
+];
---
+++ Refactored Code/out/production/Refactored Code/codemr/Refactored Code 2/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,11 @@
+var matrix = [[0,1,1],[0,0,7],[0,0,0]]
+var packages = [{
+"name": " ", "color": " #3182bd"
+}
+,{
+"name": " Views", "color": " #6baed6"
+}
+,{
+"name": " Main", "color": " #9ecae1"
+}
+];
