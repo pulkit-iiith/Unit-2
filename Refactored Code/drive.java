7feb82b5ac5c4a4a340ffc3c7d4cfd6cec84e657
Adrijac3 <30933610+Adrijac3@users.noreply.github.com>
2021-03-16 03:31:57 +0530

    UPDATE- max players made 6 (unit2)

--- Refactored Code/drive.java
+++ Refactored Code/drive.java
@@ -6,7 +6,7 @@ public class drive {
 	public static void main(String[] args) {
 
 		final int NUM_LANES = 3;
-		final int MAX_PATRONS_PER_ALLEY = 5;
+		final int MAX_PATRONS_PER_ALLEY = 6;
 
 		// Create a control desk and run the GUI
 		ControlDesk controlDesk = new ControlDesk(NUM_LANES);
